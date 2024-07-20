4668eb363280ad4eace508a2540f18fc3c5b0f68
wisdom <cocoa389@cau.ac.kr>
2024-07-21 02:42:49 +0900

    FEAT: WebConfig, QueryDSL settings

---
+++ src/main/java/global/config/QueryDSLConfig.java
@@ -0,0 +1,24 @@
+package global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QueryDSLConfig{
+
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(em);
+    }
+
+
+
+}
---
+++ src/main/java/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package global.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configurable
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowedMethods("*");
+    }
+
+
+}
