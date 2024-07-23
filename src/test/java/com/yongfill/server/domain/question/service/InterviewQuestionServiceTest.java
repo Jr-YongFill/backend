0ff09af7615c1dced11ebeb3ff92c1a7be93f6b6
hg_yellow <jang010505@gmail.com>
2024-07-23 23:34:21 +0900

    test: #14 Refactor test code
    생성하는 함수 같은 경우 재사용 가능하도록 변경

--- src/test/java/com/yongfill/server/domain/question/service/InterviewQuestionServiceTest.java
+++ src/test/java/com/yongfill/server/domain/question/service/InterviewQuestionServiceTest.java
@@ -12,7 +12,6 @@ import com.yongfill.server.global.exception.CustomException;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,32 +37,16 @@ class InterviewQuestionServiceTest {
     @Test
     @DisplayName("문제 생성 테스트")
     public void 문제_생성_테스트() {
-        Member member = Member.builder()
-                .email("email")
-                .credit(100L)
-                .attachmentFileName("123")
-                .attachmentFileSize(100L)
-                .attachmentOriginalFileName("123")
-                .filePath("123")
-                .nickname("1")
-                .password("123")
-                .createDate(LocalDateTime.now())
-                .role(Role.USER)
-                .build();
-
-        memberJpaRepository.save(member);
-
+        Member member = createMember("멤버1", Role.USER);
         Long memberId = member.getId();
-
         InterviewQuestionDto.QuestionInsertRequestDto requestDto = InterviewQuestionDto.QuestionInsertRequestDto
                 .builder()
                 .question("test 질문입니당~")
                 .build();
 
         InterviewQuestionDto.QuestionInsertResponseDto responseDto = interviewQuestionService.insertInterviewQuestion(requestDto, memberId);
-
         InterviewQuestion question = interviewQuestionJpaRepository.findById(responseDto.getQuestionId())
-                .orElseThrow(() -> new IllegalArgumentException());
+                .orElseThrow(IllegalArgumentException::new);
 
         assertEquals(responseDto.getQuestionId(), question.getId());
     }
@@ -72,7 +55,6 @@ class InterviewQuestionServiceTest {
     @DisplayName("없는 사용자가 문제 생성 테스트")
     public void 없는_사용자_예외() {
         Long memberId = 2L;
-
         InterviewQuestionDto.QuestionInsertRequestDto requestDto = InterviewQuestionDto.QuestionInsertRequestDto
                 .builder()
                 .question("test 질문입니당~")
@@ -89,27 +71,8 @@ class InterviewQuestionServiceTest {
     @Test
     @DisplayName("질문 상태 수정 N에서 Y")
     public void 질문_상태_수정_N_to_Y() {
-        Member member = Member.builder()
-                .email("email")
-                .credit(100L)
-                .attachmentFileName("123")
-                .attachmentFileSize(100L)
-                .attachmentOriginalFileName("123")
-                .filePath("123")
-                .nickname("1")
-                .password("123")
-                .createDate(LocalDateTime.now())
-                .role(Role.USER)
-                .build();
-        memberJpaRepository.save(member);
-
-        InterviewQuestion interviewQuestion = InterviewQuestion.builder()
-                .interviewShow("N")
-                .createDate(LocalDateTime.now())
-                .member(member)
-                .question("test용 질문입니디.")
-                .build();
-        interviewQuestionJpaRepository.save(interviewQuestion);
+        Member member = createMember("멤버1", Role.USER);
+        InterviewQuestion interviewQuestion = createInterviewQuestion(member, "N", "질문입니다.", null);
         Long questionId = interviewQuestion.getId();
 
         interviewQuestionService.updateQuestionState(questionId);
@@ -120,27 +83,8 @@ class InterviewQuestionServiceTest {
     @Test
     @DisplayName("질문 상태 수정 Y에서 N")
     public void 질문_상태_수정_Y_to_N() {
-        Member member = Member.builder()
-                .email("email")
-                .credit(100L)
-                .attachmentFileName("123")
-                .attachmentFileSize(100L)
-                .attachmentOriginalFileName("123")
-                .filePath("123")
-                .nickname("1")
-                .password("123")
-                .createDate(LocalDateTime.now())
-                .role(Role.USER)
-                .build();
-        memberJpaRepository.save(member);
-
-        InterviewQuestion interviewQuestion = InterviewQuestion.builder()
-                .interviewShow("Y")
-                .createDate(LocalDateTime.now())
-                .member(member)
-                .question("test용 질문입니디.")
-                .build();
-        interviewQuestionJpaRepository.save(interviewQuestion);
+        Member member = createMember("멤버1", Role.USER);
+        InterviewQuestion interviewQuestion = createInterviewQuestion(member, "Y", "질문입니다.", null);
         Long questionId = interviewQuestion.getId();
 
         interviewQuestionService.updateQuestionState(questionId);
@@ -164,27 +108,8 @@ class InterviewQuestionServiceTest {
     @Test
     @DisplayName("질문 삭제")
     public void 질문_삭제() {
-        Member member = Member.builder()
-                .email("email")
-                .credit(100L)
-                .attachmentFileName("123")
-                .attachmentFileSize(100L)
-                .attachmentOriginalFileName("123")
-                .filePath("123")
-                .nickname("1")
-                .password("123")
-                .createDate(LocalDateTime.now())
-                .role(Role.USER)
-                .build();
-        memberJpaRepository.save(member);
-
-        InterviewQuestion interviewQuestion = InterviewQuestion.builder()
-                .interviewShow("Y")
-                .createDate(LocalDateTime.now())
-                .member(member)
-                .question("test용 질문입니디.")
-                .build();
-        interviewQuestionJpaRepository.save(interviewQuestion);
+        Member member = createMember("멤버1", Role.USER);
+        InterviewQuestion interviewQuestion = createInterviewQuestion(member, "N", "질문입니다.", null);
         Long questionId = interviewQuestion.getId();
 
         interviewQuestionService.deleteQuestion(questionId);
@@ -193,47 +118,13 @@ class InterviewQuestionServiceTest {
         assertEquals(false, exists);
     }
 
-
     @Test
     @DisplayName("질문 스택 변경")
     public void 질문_스택_변경() {
-        Member member = Member.builder()
-                .email("email")
-                .credit(100L)
-                .attachmentFileName("123")
-                .attachmentFileSize(100L)
-                .attachmentOriginalFileName("123")
-                .filePath("123")
-                .nickname("1")
-                .password("123")
-                .createDate(LocalDateTime.now())
-                .role(Role.USER)
-                .build();
-        memberJpaRepository.save(member);
-
-        QuestionStack originStack = QuestionStack.builder()
-                .price(0L)
-                .stackName("자바")
-                .description("자바칩 프라푸치노")
-                .build();
-        questionStackJpaRepository.save(originStack);
-
-        InterviewQuestion interviewQuestion = InterviewQuestion.builder()
-                .interviewShow("Y")
-                .createDate(LocalDateTime.now())
-                .member(member)
-                .question("test용 질문입니디.")
-                .questionStack(originStack)
-                .build();
-        interviewQuestionJpaRepository.save(interviewQuestion);
-
-        QuestionStack stack = QuestionStack.builder()
-                .price(0L)
-                .stackName("리엑트")
-                .description("Re-act")
-                .build();
-        questionStackJpaRepository.save(stack);
-
+        Member member = createMember("멤버1", Role.USER);
+        QuestionStack originStack =  createStack("자바");
+        InterviewQuestion interviewQuestion = createInterviewQuestion(member, "N", "질문입니다.", originStack);
+        QuestionStack stack =  createStack("리엑트");
         Long questionId = interviewQuestion.getId();
         Long stackId = stack.getId();
 
@@ -245,35 +136,9 @@ class InterviewQuestionServiceTest {
     @Test
     @DisplayName("질문 스택 등록")
     public void 질문_스택_등록() {
-        Member member = Member.builder()
-                .email("email")
-                .credit(100L)
-                .attachmentFileName("123")
-                .attachmentFileSize(100L)
-                .attachmentOriginalFileName("123")
-                .filePath("123")
-                .nickname("1")
-                .password("123")
-                .createDate(LocalDateTime.now())
-                .role(Role.USER)
-                .build();
-        memberJpaRepository.save(member);
-
-        InterviewQuestion interviewQuestion = InterviewQuestion.builder()
-                .interviewShow("Y")
-                .createDate(LocalDateTime.now())
-                .member(member)
-                .question("test용 질문입니디.")
-                .build();
-        interviewQuestionJpaRepository.save(interviewQuestion);
-
-        QuestionStack stack = QuestionStack.builder()
-                .price(0L)
-                .stackName("자바")
-                .description("자바칩 프라푸치노")
-                .build();
-        questionStackJpaRepository.save(stack);
-
+        Member member = createMember("멤버1", Role.USER);
+        InterviewQuestion interviewQuestion = createInterviewQuestion(member, "N", "질문입니다.", null);
+        QuestionStack stack = createStack("자바");
         Long questionId = interviewQuestion.getId();
         Long stackId = stack.getId();
 
@@ -285,27 +150,7 @@ class InterviewQuestionServiceTest {
     @Test
     @DisplayName("없는 질문 스택 등록")
     public void 없는_질문_스택_등록() {
-        Member member = Member.builder()
-                .email("email")
-                .credit(100L)
-                .attachmentFileName("123")
-                .attachmentFileSize(100L)
-                .attachmentOriginalFileName("123")
-                .filePath("123")
-                .nickname("1")
-                .password("123")
-                .createDate(LocalDateTime.now())
-                .role(Role.USER)
-                .build();
-        memberJpaRepository.save(member);
-
-        QuestionStack stack = QuestionStack.builder()
-                .price(0L)
-                .stackName("자바")
-                .description("자바칩 프라푸치노")
-                .build();
-        questionStackJpaRepository.save(stack);
-
+        QuestionStack stack = createStack("자바");
         Long questionId = 1L;
         Long stackId = stack.getId();
 
@@ -320,28 +165,8 @@ class InterviewQuestionServiceTest {
     @Test
     @DisplayName("질문에 없는 스택 등록")
     public void 질문에_없는_스택_등록() {
-        Member member = Member.builder()
-                .email("email")
-                .credit(100L)
-                .attachmentFileName("123")
-                .attachmentFileSize(100L)
-                .attachmentOriginalFileName("123")
-                .filePath("123")
-                .nickname("1")
-                .password("123")
-                .createDate(LocalDateTime.now())
-                .role(Role.USER)
-                .build();
-        memberJpaRepository.save(member);
-
-        InterviewQuestion interviewQuestion = InterviewQuestion.builder()
-                .interviewShow("Y")
-                .createDate(LocalDateTime.now())
-                .member(member)
-                .question("test용 질문입니디.")
-                .build();
-        interviewQuestionJpaRepository.save(interviewQuestion);
-
+        Member member = createMember("멤버1", Role.USER);
+        InterviewQuestion interviewQuestion = createInterviewQuestion(member, "N", "질문입니다.", null);
         Long questionId = interviewQuestion.getId();
         Long stackId = 1L;
 
@@ -352,4 +177,47 @@ class InterviewQuestionServiceTest {
 
         assertEquals(INVALID_STACK.getMessage(), exception.getMessage());
     }
+
+    private Member createMember(String name, Role role) {
+        Member member = Member.builder()
+                .email(name)
+                .credit(0L)
+                .attachmentFileName(name)
+                .attachmentFileSize(0L)
+                .attachmentOriginalFileName(name)
+                .filePath(name)
+                .nickname(name)
+                .password(name)
+                .createDate(LocalDateTime.now())
+                .role(role)
+                .build();
+        memberJpaRepository.save(member);
+
+        return member;
+    }
+
+    private InterviewQuestion createInterviewQuestion(Member member, String state, String content, QuestionStack stack) {
+        InterviewQuestion interviewQuestion = InterviewQuestion.builder()
+                .interviewShow(state)
+                .createDate(LocalDateTime.now())
+                .member(member)
+                .question(content)
+                .questionStack(stack)
+                .build();
+        interviewQuestionJpaRepository.save(interviewQuestion);
+
+        return interviewQuestion;
+    }
+
+    private QuestionStack createStack(String name) {
+        QuestionStack stack = QuestionStack.builder()
+                .price(0L)
+                .stackName(name)
+                .description(name)
+                .build();
+        questionStackJpaRepository.save(stack);
+
+        return stack;
+    }
+
 }
