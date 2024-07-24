c8b13848ec162bdb31ab446142ff6f08b672731a
wisdom <cocoa389@naver.com>
2024-07-25 00:08:25 +0900

    Refactor: Post의 Create부분 구조 개편
    * Dto CRUD별 분리 예정
    * Service interface 사용예정

--- src/main/java/com/yongfill/server/domain/posts/api/PostAPI.java
+++ src/main/java/com/yongfill/server/domain/posts/api/PostAPI.java
@@ -1,34 +1,41 @@
 package com.yongfill.server.domain.posts.api;
 
-import com.yongfill.server.domain.posts.dto.PostDto;
-import com.yongfill.server.domain.posts.service.PostService;
+import com.yongfill.server.domain.posts.dto.CreatePostDto;
+import com.yongfill.server.domain.posts.service.PostServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class PostAPI {
 
-    private final PostService postService;
+    private final PostServiceImpl postService;
 
     @PostMapping("/api/posts")
-    public ResponseEntity<PostDto.PostResponseDto> savePost(@RequestBody PostDto.PostRequestDto postRequestDto){
+    public ResponseEntity<CreatePostDto.ResponseDto> savePost(@RequestBody CreatePostDto.RequestDto createRequestDto){
 
         HttpStatus status = HttpStatus.CREATED;
-        PostDto.PostResponseDto postResponseDto = postService.createPost(postRequestDto);
+        CreatePostDto.ResponseDto createResponseDto = postService.createPost(createRequestDto);
 
-        return new ResponseEntity<>(postResponseDto, status);
+        return new ResponseEntity<>(createResponseDto, status);
     }
 
-    @GetMapping("/api/categories/{category_name}/posts")
-    public ResponseEntity<List<PostDto.PostResponseDto>> findAllByCategoryName(@PathVariable("category_name") String categoryName){
-        HttpStatus status = HttpStatus.CREATED;
-        List <PostDto.PostResponseDto> postResponseDto = postService.findAllByCategory(categoryName);
+//    @GetMapping("/api/categories/{category_name}/posts")
+//    public ResponseEntity<List<PostDto.PostResponseDto>> findAllByCategoryName(@PathVariable("category_name") String categoryName){
+//        HttpStatus status = HttpStatus.CREATED;
+//        List <PostDto.PostResponseDto> postResponseDto = postService.findAllByCategory(categoryName);
+//
+//        return new ResponseEntity<>(postResponseDto,status);
+//    }
+//
+//    @PatchMapping("/api/posts/{post_id}")
+//    public ResponseEntity<PostDto.PostResponseDto> updatePost(@PathVariable("post_id") Long postId,@RequestBody PostDto.CreateRequestDto createRequestDto){
+//        HttpStatus status = HttpStatus.CREATED;
+//        PostDto.PostResponseDto postResponseDto= postService.updatePost(postId, createRequestDto);
+//        return new ResponseEntity<>(postResponseDto,status);
+//    }
 
-        return new ResponseEntity<>(postResponseDto,status);
-    }
 }
