c378bcb68fcd45dcc97e26ef84c58002db3392ce
hg_yellow <jang010505@gmail.com>
2024-07-24 10:46:12 +0900

    feat: #23 Add stack insert api
    [POST] /api/stacks API 작성 완료

--- src/main/java/com/yongfill/server/domain/stack/service/QuestionStackService.java
+++ src/main/java/com/yongfill/server/domain/stack/service/QuestionStackService.java
@@ -5,12 +5,14 @@ import com.yongfill.server.domain.stack.repository.QuestionStackJpaRepository;
 import com.yongfill.server.domain.stack.dto.QuestionStackDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionStackService {
     private final QuestionStackJpaRepository questionStackJpaRepository;
 
+    @Transactional
     public QuestionStackDto.StackInsertResponseDto insertStack(QuestionStackDto.StackInsertRequestDto requestDto) {
         QuestionStack stack = requestDto.toEntity();
         questionStackJpaRepository.save(stack);
