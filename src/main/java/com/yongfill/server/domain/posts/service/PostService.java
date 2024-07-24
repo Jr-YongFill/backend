29d8f5bc31396dd767607bb3b7bcde4b0452a803
wisdom <cocoa389@naver.com>
2024-07-24 14:43:27 +0900

    Feat: 카테고리별 게시글 조회
    카테고리 이름은 INFO, QNA로 집어넣으시면 됩니다.

--- src/main/java/com/yongfill/server/domain/posts/service/PostService.java
+++ src/main/java/com/yongfill/server/domain/posts/service/PostService.java
@@ -6,14 +6,17 @@ import com.yongfill.server.domain.posts.dto.PostDto;
 import com.yongfill.server.domain.posts.entity.Category;
 import com.yongfill.server.domain.posts.entity.Post;
 import com.yongfill.server.domain.posts.repository.PostJpaRepository;
+import com.yongfill.server.domain.posts.repository.PostQueryDSLRepositoryImpl;
 import com.yongfill.server.global.common.response.error.ErrorCode;
 import com.yongfill.server.global.exception.CustomException;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +27,7 @@ public class PostService {
 
 
 
+    //C
     @Transactional
     public PostDto.PostResponseDto createPost(PostDto.PostRequestDto postRequestDto){
         // Member를 조회하고 예외 처리
@@ -36,6 +40,24 @@ public class PostService {
 
         return entityToDto(post);
     }
+
+    //R
+    @Transactional(readOnly = true)
+    public List<PostDto.PostResponseDto> findAllByCategory(String categoryName) {
+
+        Category category = Category.valueOf(categoryName);
+        List<Post> posts = postJpaRepository.findAllByCategory(category);
+
+        return posts.stream()
+                .map(this::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+    //U
+
+    //D
+
+
     //엔터티 DTO로
     private PostDto.PostResponseDto entityToDto(Post post){
         return PostDto.PostResponseDto.builder()
