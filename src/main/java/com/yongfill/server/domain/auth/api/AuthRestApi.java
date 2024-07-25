ef61ecc7d6782601cbd9bec2f00c1afddb24172f
Changchang <bbmini96@hanmail.net>
2024-07-25 16:50:45 +0900

    remove #24 files

--- src/main/java/com/yongfill/server/domain/auth/api/AuthRestApi.java
+++
@@ -1,2 +0,0 @@
-package com.yongfill.server.domain.auth.controller;public class AuthRestController {
-}
--- src/main/java/com/yongfill/server/domain/auth/service/AuthService.java
+++
@@ -1,2 +0,0 @@
-package com.yongfill.server.domain.auth.service;public class AuthService {
-}
