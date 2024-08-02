0d8062f85ccc85ee9ea114cb50854fb6d21dce2e
hg_yellow <63451752+jang010505@users.noreply.github.com>
2024-08-02 17:22:38 +0900

    Update WebConfig.java

--- src/main/java/com/yongfill/server/global/config/WebConfig.java
+++ src/main/java/com/yongfill/server/global/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowCredentials(true)
+                .allowedOrigins("http://3.36.100.244:80")
                 .maxAge(3600)
                 .allowedMethods("*");
     }
