790120870de54684a5fb16e3e95b19cd77fa62e2
wisdom <cocoa389@naver.com>
2024-08-06 09:37:04 +0900

    fIx:로컬환경 롤백

--- src/main/java/com/yongfill/server/global/config/WebConfig.java
+++ src/main/java/com/yongfill/server/global/config/WebConfig.java
@@ -11,7 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://www.mosi.digital")
                 .allowCredentials(true)
                 .maxAge(3600)
                 .allowedMethods("*");
